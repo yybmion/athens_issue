42406b75c31650ead754917e0626be4207a89d4c
yybmion <yyb400@ajou.ac.kr>
2024-07-16 19:06:54 +0900

    테스트 커밋

---
+++ src/main/java/com/example/athens_issue/AthensIssueApplication.java
@@ -0,0 +1,13 @@
+package com.example.athens_issue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AthensIssueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AthensIssueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/athens_issue/AthensIssueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.athens_issue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AthensIssueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
